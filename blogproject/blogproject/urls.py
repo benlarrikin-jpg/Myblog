2b581d98c58ab80b253282ba713685c5abe8dca8
benlarrikin-jpg <benlarrikin@gmail.com>
2025-11-03 14:25:26 +0000

    emeka

--- blogproject/blogproject/urls.py
+++ blogproject/blogproject/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('emeka/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('', views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
